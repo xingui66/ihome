3abf70b561dc6de1288dd02a9675f366e1153a0f
xingui66 <xingui66@126.com>
2019-11-10 00:50:13 -0800

    初始化mysql

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-package ihome
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"ihomegit/ihome/model"
+)
+
+func main() {
+	//初始化路由
+	//路由分组
+	router:= gin.Default()
+	r0 := router.Group("/v0")
+	{
+		r0.GET("abc", func(ctx *gin.Context) {
+			ctx.Writer.WriteString("abcdefg")
+		})
+	}
+
+	r1:=router.Group("/v1")
+	{
+		r1.GET("edf", func(ctx *gin.Context) {
+			ctx.Writer.WriteString("喵喵")
+		})
+	}
+
+	model.InitModel()
+    //model.InsertData();
+
+}
---
+++ model/model.go
@@ -0,0 +1,35 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+	"fmt"
+)
+
+type Stu struct{
+	gorm.Model
+	Name string
+	PassWord string
+}
+
+var GlobalDb *gorm.DB
+
+func InitModel(){
+	db ,err:= gorm.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/ihome?parseTime=true")
+	if err != nil {
+		fmt.Println("连接数据库失败")
+		return
+	}
+
+	//连接池设置
+	db.DB().SetMaxIdleConns(20)
+	db.DB().SetMaxOpenConns(30)
+	db.DB().SetConnMaxLifetime(60 * 30)
+
+	//设置表名为单数形式
+	db.SingularTable(true)
+	GlobalDb = db
+
+	//自动迁移  在gorm中建表默认是负数形式
+	db.AutoMigrate(new(Stu))
+
+}
